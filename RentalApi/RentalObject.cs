61d873c8c7ff231f00d7a65763aceeaa66b167e3
Kyle Buck <kpbuck91@gmail.com>
2022-04-21 19:19:34 -0500

    Added RentalObject

--- RentalApi/RentalObject.cs
+++ RentalApi/RentalObject.cs
@@ -2,5 +2,16 @@
 {
     public class RentalObject
     {
+        public RentalObject(string name)
+        {
+            ID = new Guid();
+            Name = name;
+
+        }
+
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+
+
     }
 }
