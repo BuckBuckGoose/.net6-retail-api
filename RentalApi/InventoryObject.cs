a2d2e61383ebc6b82b5d5cb6e44b9d4096e68355
Kyle Buck <kpbuck91@gmail.com>
2022-09-09 17:09:36 -0500

    Updated commit

---
+++ RentalApi/InventoryObject.cs
@@ -0,0 +1,22 @@
+﻿namespace RentalApi
+{
+    public class InventoryObject
+    {
+        public InventoryObject(Guid guid, string name)
+        {
+            ID = guid;
+            Name = name;
+
+        }
+
+        public Guid ID { get; private set; }
+        public string Name { get; set; }
+
+        public static InventoryObject Create(string name)
+        {
+            return new InventoryObject(Guid.NewGuid(), name);
+        }
+
+
+    }
+}
---
+++ RentalApiTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RentalApiTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
